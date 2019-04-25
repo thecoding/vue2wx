9ea1c936d8359e39454248b5e7f9fa88ce1f5699
Mirko <zhouchao180@gmail.com>
2019-04-25 18:01:12 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue(App)
+app.$mount()
+
+
+export default{
+  config: {
+    pages: [],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor:'"#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black',
+    },
+    tabBar: {
+      // "color": "#999",
+      // "backgroundColor": "#fafafa",
+      // "selectedColor": "#333",
+      // "borderStyle": "white",
+  
+      list: [{
+        text: '自选',
+        pagePath: 'pages/index/main',
+        iconPath: 'static/tabs/zxg.png',
+        selectedIconPath: 'static/tabs/zxg_1.png'
+      }, {
+        text: '行情',
+        pagePath: 'pages/information/main',
+        iconPath: 'static/tabs/hq.png',
+        selectedIconPath: 'static/tabs/hq_1.png',
+      }, {
+        text: '资讯',
+        pagePath: 'pages/otherInfo/main',
+        iconPath: 'static/tabs/zx.png',
+        selectedIconPath: 'static/tabs/zx_1.png',
+      }, {
+        text: '我的',
+        pagePath: 'pages/account/main',
+        iconPath: 'static/tabs/yh.png',
+        selectedIconPath: 'static/tabs/yh_1.png',
+      }],
+      position: 'bottom'
+    }
+  }  
+}
---
+++ src/pages/choise/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="counter-warp">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      msg: "自选"
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
